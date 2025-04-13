fe694a65d055c74f0af0db5ba6423c151cbfa0aa
Maroti Uppe <marotiuppe891@gmail.com>
2025-04-13 19:19:42 +0530

    Fix: Avatar Not Loading

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import defaultImage from '../assets/images/default-avatar.jpg';
 import { useData } from '../context/dataContext';
 import { useParams } from 'react-router-dom';
@@ -6,8 +6,23 @@ import { useParams } from 'react-router-dom';
 const Avatar = () => {
   const { profileId } = useParams();
   const { getProfileByProfileId } = useData();
-  
-  const profile = getProfileByProfileId(profileId);
+
+  const [profile, setProfile] = useState(null);  
+  const [loading, setLoading] = useState(true);  
+
+  useEffect(() => {  
+    const fetchProfile = async () => {  
+      const fetchedProfile = await getProfileByProfileId(profileId);  
+      setProfile(fetchedProfile);  
+      setLoading(false);  
+    };  
+    
+    fetchProfile();  
+  }, [profileId, getProfileByProfileId]);  
+
+  if (loading) {  
+    return <div>Loading Avatar...</div>;  
+  }
 
   if (!profile) {
     return <div>Profile not available</div>;
